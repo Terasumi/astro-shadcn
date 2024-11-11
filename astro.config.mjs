a024a59515c3831767cb97772306665a72c91edd
tongducthanhnam <tongducthanhnam@gmail.com>
2024-11-11 21:05:09 +0700

    ISR: Change routes structure to adapt ISR.

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,29 +13,32 @@ import vtbot from 'astro-vtbot';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-      applyBaseStyles: false,
-  }), vtbot(
-      {
-          loadingIndicator: true
-      }
-  )],
+    integrations: [react(),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        // vtbot(
+        //     {
+        //         loadingIndicator: true
+        //     }
+        // ),
+    ],
 
-  image: {
-      domains: ['wsrv.nl', "phimimg.com"],
-  },
+    image: {
+        domains: ['wsrv.nl', "phimimg.com"],
+    },
 
-  output: "server",
-  adapter: vercel(
-      {
-          webAnalytics: {
-              enabled: true,
-          },
-          isr: {
-              // caches all pages on first request and saves for 1 day
-              expiration: 60 * 60 * 24,
-              exclude: ['/'], // Loại trừ trang chủ khỏi ISR
-          },
-      }
-  ),
+    output: "server",
+    adapter: vercel(
+        {
+            webAnalytics: {
+                enabled: true,
+            },
+            isr: {
+                // caches all pages on first request and saves for 6 hours
+                expiration: 60 * 60 * 6,
+                // exclude: ['/'], // Loại trừ trang chủ khỏi ISR
+            },
+        }
+    ),
 });
