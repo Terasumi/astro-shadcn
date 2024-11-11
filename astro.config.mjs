9fe3eedb344b8ba37e2e3a1d9dcdbc5285e19380
tongducthanhnam <tongducthanhnam@gmail.com>
2024-11-11 14:53:52 +0700

    Add View Transition -> ProgressBar

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,17 @@ import vercel from '@astrojs/vercel/serverless';
 
 import node from '@astrojs/node';
 
+import vtbot from 'astro-vtbot';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({
       applyBaseStyles: false,
-  }),],
+  }), vtbot(
+      {
+          loadingIndicator: true
+      }
+  )],
 
   image: {
       domains: ['wsrv.nl', "phimimg.com"],
